5e3a881fee05e4be9b7a675a068f08bc716b1e72
clinton58801 <CLINTON1972@OUTLOOK.COM>
2025-07-24 11:08:55 -0500

    Delete src directory

--- src/components/ui/button.jsx
+++
@@ -1,7 +0,0 @@
-export function Button({ children, ...props }) {
-  return (
-    <button {...props} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-      {children}
-    </button>
-  );
-}
